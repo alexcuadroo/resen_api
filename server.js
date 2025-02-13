4962343c196ecd8eb920b8e48b41498588e2126c
alexcuadroo <com.alexcuadro@gmail.com>
2025-02-13 08:24:17 -0300

    Eliminar archivo index.html y actualizar package.json para usar server.js como entrada principal; agregar dependencias de express y cors. Implementar servidor básico con ruta para enviar correos electrónicos.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:4321, https://edualex.uy',
+    methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type'], // Encabezados permitidos
+}));
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+// Tu ruta de la API
+app.post('/api/send-email', (req, res) => {
+    // Lógica para manejar la solicitud
+    res.json({ success: true });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+});
